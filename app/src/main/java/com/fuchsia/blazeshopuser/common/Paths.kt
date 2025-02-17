d93dd0d3804c003dbe820f7dc1e5d6840f08a243
juyelmajumder <justjuyel@gmail.com>
2025-02-17 20:49:03 +0530

    Final commit

---
+++ app/src/main/java/com/fuchsia/blazeshopuser/common/Paths.kt
@@ -0,0 +1,13 @@
+package com.fuchsia.blazeshopuser.common
+
+const val CATEGORY_PATH = "category"
+
+const val RAZORPAY_API_PATH = "rzp_test_62kLoQuo4Mv4dK"
+
+const val USER_PATH = "Users"
+
+const val FLASH_SALE = "Flash Sale"
+
+const val BANNERS_PATH = "Banners"
+
+const val ONE_SIGNAL_ID = "3eac0d55-0c78-4ef5-b289-cb8282e85c5c"
