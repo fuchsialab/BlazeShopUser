6fbd0502c4bab170d62282e1add0ee7c28acf8ef
Juyel Majumder <49267178+fuchsialab@users.noreply.github.com>
2025-02-28 15:38:16 +0530

    Update Paths.kt

--- app/src/main/java/com/fuchsia/blazeshopuser/common/Paths.kt
+++ app/src/main/java/com/fuchsia/blazeshopuser/common/Paths.kt
@@ -2,7 +2,7 @@ package com.fuchsia.blazeshopuser.common
 
 const val CATEGORY_PATH = "category"
 
-const val RAZORPAY_API_PATH = "rzp_test_62kLoQuo4Mv4dK"
+const val RAZORPAY_API_PATH = ""
 
 const val USER_PATH = "Users"
 
@@ -10,4 +10,4 @@ const val FLASH_SALE = "Flash Sale"
 
 const val BANNERS_PATH = "Banners"
 
-const val ONE_SIGNAL_ID = "3eac0d55-0c78-4ef5-b289-cb8282e85c5c"
+const val ONE_SIGNAL_ID = ""
